02a11a94d3434d96a576a305565f65238c67a1ea
John Grimes <john@grimes.id.au>
2021-11-22 14:32:42 +1000

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import {render} from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import {ThemeProvider} from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
-import {oauth2 as SMART} from "fhirclient";
-import {IPatient} from "@ahryman40k/ts-fhir-types/lib/R4";
+import * as React from "react";
+import { render } from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import theme from "./theme";
+import { oauth2 as SMART } from "fhirclient";
+import { IPatient } from "@ahryman40k/ts-fhir-types/lib/R4";
 
-let rootElement = document.querySelector('#root');
+let rootElement = document.querySelector("#root");
 
 SMART.init({
+  iss:
+    "https://launch.smarthealthit.org/v/r3/sim/eyJoIjoiMSIsImIiOiJzbWFydC0xNjQyMDY4IiwiZSI6InNtYXJ0LVByYWN0aXRpb25lci03MTYxNDUwMiJ9/fhir",
   redirectUri: "test.html",
   clientId: "whatever",
   scope: "launch/patient offline_access openid fhirUser",
@@ -20,15 +22,16 @@ SMART.init({
   // setting this!
   completeInTarget: true
 })
-.then((client) =>
-    client.request<IPatient>(`/Patient/${client.patient.id}`)
-)
-.then(
+  .then((client) => client.request<IPatient>(`/Patient/${client.patient.id}`))
+  .then(
     (patient) => {
-      render(<ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App patient={patient} />
-      </ThemeProvider>, rootElement);
+      render(
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App patient={patient} />
+        </ThemeProvider>,
+        rootElement
+      );
     },
     (error) => console.error(error)
-);
+  );
