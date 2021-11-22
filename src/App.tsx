18bbaf35fd227735f98cf7eb4af7df0a1548f878
John Grimes <John.Grimes@csiro.au>
2021-11-22 16:25:40 +1000

    Rough initial implementation of form

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import Typography from "@mui/material/Typography";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
-import {Stack, TextField} from "@mui/material";
-import {IPatient, PatientGenderKind} from "@ahryman40k/ts-fhir-types/lib/R4";
+import {
+  Card,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { IPatient, PatientGenderKind } from "@ahryman40k/ts-fhir-types/lib/R4";
 import moment from "moment";
 
 interface Props {
@@ -13,22 +20,118 @@ interface Props {
 }
 
 export default function App(props: Props) {
-  const {patient} = props,
-  birthSexIsMale = patient.gender === PatientGenderKind._male,
-  age = moment().diff(patient.birthDate, 'years');
+  const { patient } = props,
+    birthSexIsMale = patient.gender === PatientGenderKind._male,
+    age = moment().diff(patient.birthDate, "years");
+
+  const FormField = (props: { children: any }) => (
+    <FormControl fullWidth>{props.children}</FormControl>
+  );
+
+  console.log("Patient", patient);
 
   return (
     <Container maxWidth="sm">
-      <Stack spacing={2} sx={{ my: 4 }}>
+      <Stack spacing={4} sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Cardiovascular risk calculator
         </Typography>
-        <FormControl fullWidth>
-          <FormControlLabel label="Birth sex is male?" control={<Switch />} value={birthSexIsMale} />
-        </FormControl>
-        <FormControl fullWidth>
-          <TextField label="Age in years" type="number" value={age}/>
-        </FormControl>
+        <FormField>
+          <InputLabel id="birth-sex">Biological sex at birth</InputLabel>
+          <Select label="birth-sex" required>
+            <MenuItem value="248152002">Female</MenuItem>
+            <MenuItem value="248153007">Male</MenuItem>
+            <MenuItem value="32570691000036108">Intersex</MenuItem>
+            <MenuItem value="32570681000036106">Indeterminate sex</MenuItem>
+          </Select>
+        </FormField>
+        <FormField>
+          <TextField label="Age in years" type="number" value={age} required />
+        </FormField>
+        <FormField>
+          <InputLabel id="ethnicity">Ethnicity</InputLabel>
+          <Select label="ethnicity" required>
+            <MenuItem value="european">European</MenuItem>
+            <MenuItem value="maori">Maori</MenuItem>
+            <MenuItem value="pacific">Pacific</MenuItem>
+            <MenuItem value="indian">Indian</MenuItem>
+            <MenuItem value="asian">Asian</MenuItem>
+            <MenuItem value="other">Other</MenuItem>
+          </Select>
+        </FormField>
+        <FormField>
+          <TextField
+            label="Ratio of total cholesterol / HDL"
+            type="number"
+            required
+            inputProps={{ min: 0, max: 1, step: 0.1 }}
+          />
+        </FormField>
+        <FormField>
+          <TextField
+            label="Systolic blood pressure (mm Hg)"
+            type="number"
+            required
+            inputProps={{ min: 0 }}
+          />
+        </FormField>
+        <FormField>
+          <TextField
+            label="New Zealand Index of Socioeconomic Deprivation score"
+            type="number"
+            required
+            inputProps={{ min: 0, max: 10 }}
+          />
+        </FormField>
+        <FormField>
+          <InputLabel id="smoking">Smoking status</InputLabel>
+          <Select label="smoking" required>
+            <MenuItem value="8392000">Non-smoker</MenuItem>
+            <MenuItem value="160617001">Stopped smoking</MenuItem>
+            <MenuItem value="77176002">Current smoker</MenuItem>
+          </Select>
+        </FormField>
+        <FormField>
+          <FormControlLabel
+            label="Family history of cardiovascular disease?"
+            control={<Switch />}
+          />
+        </FormField>
+        <FormField>
+          <FormControlLabel
+            label="History of atrial fibrillation?"
+            control={<Switch />}
+          />
+        </FormField>
+        <FormField>
+          <FormControlLabel label="History of diabetes?" control={<Switch />} />
+        </FormField>
+        <FormField>
+          <FormControlLabel
+            label="Currently taking blood pressure lowering medication?"
+            control={<Switch />}
+          />
+        </FormField>
+        <FormField>
+          <FormControlLabel
+            label="Currently taking lipid lowering medication?"
+            control={<Switch />}
+          />
+        </FormField>
+        <FormField>
+          <FormControlLabel
+            label="Currently taking anti-thrombotic medication?"
+            control={<Switch />}
+          />
+        </FormField>
+        <Card variant="outlined" sx={{ p: 2 }}>
+          <Stack direction="row" alignItems="center">
+            <Typography variant="h6" component="h2" sx={{ flexGrow: 1 }}>
+              Calculated risk score
+            </Typography>
+            <Typography color="primary">(not enough information)</Typography>
+          </Stack>
+        </Card>
       </Stack>
     </Container>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,29 @@
-import * as React from 'react';
-import {render} from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import {ThemeProvider} from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
-import {oauth2 as SMART} from "fhirclient";
-import {IPatient} from "@ahryman40k/ts-fhir-types/lib/R4";
+import * as React from "react";
+import { render } from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import theme from "./theme";
+import { oauth2 as SMART } from "fhirclient";
+import { IPatient } from "@ahryman40k/ts-fhir-types/lib/R4";
 
-let rootElement = document.querySelector('#root');
+let rootElement = document.querySelector("#root");
 
 SMART.init({
   redirectUri: "test.html",
   clientId: "whatever",
   scope: "launch/patient offline_access openid fhirUser",
-
-  // WARNING: completeInTarget=true is needed to make this work in the codesandbox
-  // frame. It is otherwise not needed if the target is not another frame or window
-  // but since the entire example works in a frame here, it gets confused without
-  // setting this!
-  completeInTarget: true
 })
-.then((client) =>
-    client.request<IPatient>(`/Patient/${client.patient.id}`)
-)
-.then(
+  .then((client) => client.request<IPatient>(`/Patient/${client.patient.id}`))
+  .then(
     (patient) => {
-      render(<ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App patient={patient} />
-      </ThemeProvider>, rootElement);
+      render(
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App patient={patient} />
+        </ThemeProvider>,
+        rootElement
+      );
     },
     (error) => console.error(error)
-);
+  );
